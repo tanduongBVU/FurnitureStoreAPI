31e4cd72fd8e67820794a409a1b890731bf01d07
Nguyen Tan Duong <duongnguyentan800@gmail.com>
2026-08-01 09:47:09 +0700

    Initial commit

---
+++ FurnitureStoreAPI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using FurnitureStoreAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_context.Products.ToList());
+        }
+    }
+}
---
+++ FurnitureStoreAPI/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace FurnitureStoreAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Image { get; set; } = string.Empty;
+
+        public int Stock { get; set; }
+    }
+}
